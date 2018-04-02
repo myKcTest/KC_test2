fc003296e8db63679cdec0d667a0424a74d586af
q <q@q.er>
2018-04-02 12:14:46 +0300

    qweqwe

---
+++ TC_1576918/test.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function test(){
+	if(!undefined){
+		console.log("Always Printed!!!!");
+	}
+	if(undefined){
+	}
+}
---
+++ TC_1576918/test.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sample Test document</title>
+    <style>
+		.abc{
+			/* one line css */	width: 100px;	}
+		.bcd{
+			/* Two line css */
+		}
+    </style>
+</head>
+<body>
+	<p>
+    1 line content</p>
+    <p>
+    2 line
+    content </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, consequuntur, non, voluptatum distinctio dolore numquam quas sint illo sapiente accusantium ullam quam voluptatibus neque corporis corrupti sed commodi ipsa perspiciatis ipsam at odio excepturi repudiandae explicabo? Adipisci, architecto, recusandae, assumenda alias veniam laboriosam nemo neque nesciunt ipsum sed itaque magnam consequuntur repellendus maxime fugit eligendi at nulla praesentium sequi totam cupiditate obcaecati quos expedita animi ex tempore repellat id rem fugiat ad optio reiciendis minus delectus deleniti dolorem quaerat numquam. Perspiciatis, labore ipsum qui harum placeat maxime voluptate inventore assumenda quam a reiciendis non facere consequatur omnis delectus nam tempora.</p>
+    
+    <?php
+	?>
+    <?php
+	
+	?>
+</body>
+</html>
